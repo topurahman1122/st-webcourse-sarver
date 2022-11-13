a8b0f43d56af688153aa1d08639ca49d335fa360
topurahman <topu.rahman24@gmail.com>
2022-11-13 23:13:58 +0600

    update

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+//middleware
+app.use(cors());
+app.use(express.json());
 
 const catedories = require('./data/category.json');
 
@@ -15,5 +18,5 @@ app.get('/news-categories', (req, res) => {
     res.send(catedories);
 })
 app.listen(port, () => {
-    console.log('st coourse port', port);
+    console.log('st course port', port);
 })
