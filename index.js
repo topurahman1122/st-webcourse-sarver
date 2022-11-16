d2656cf34a012ddfa8ac5b07fcdd616f02d58062
topurahman <topu.rahman24@gmail.com>
2022-11-16 13:31:18 +0600

    update

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 const catedories = require('./data/category.json');
-
+const news = require('./data/news.json');
 
 app.get('/', (req, res) => {
     res.send('st api is running');
@@ -17,6 +17,28 @@ app.get('/', (req, res) => {
 app.get('/news-categories', (req, res) => {
     res.send(catedories);
 })
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news);
+    }
+    else {
+        const category_news = news.filter(n => n.category_id === id);
+        res.send(category_news);
+    }
+})
+
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n = n._id === id);
+    res.send(selectedNews);
+})
+
 app.listen(port, () => {
     console.log('st course port', port);
 })
